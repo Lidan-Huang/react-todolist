b2f205ea0494f8164ea53c14769bc375d05d6d19
Lidan-Huang <huangld0314@gmail.com>
2022-10-17 21:58:44 -0700

    added two solutions of fetchAPIs in QuoteApp component

--- src/QuoteApp.js
+++ src/QuoteApp.js
@@ -7,17 +7,33 @@ function QuoteApp() {
   const [isDisplay, setIsDisplay] = useState(false);
   const [quote, setQuote] = useState({"text": "", "author": ""});
 
-  async function toggleDisplay() {
+  function toggleDisplay() {
     let display = !isDisplay;
     setIsDisplay(display);
-    await getQuote();
+    //solution1: async function toggleDisplay(){await getQuote();}
+    //solution2: getQuote2(); no async and await
+    getQuote2();
   }
 
   async function getQuote() {
     let response = await fetch(randomQuoteUrl);
     let newQuote = await response.json();
-    console.log("newQuote*********", newQuote.quote);
+    console.log("newQuote*********", newQuote);
     setQuote(quote => ({...newQuote.quote}));
+  }
+
+  function getQuote2() {
+    fetch(randomQuoteUrl).then((response) =>{
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(response.statusText);
+      }
+    }).then((data) => {
+      setQuote(quote => ({...data.quote}))
+    }).catch((error) => {
+      console.log("Error:", error);
+    });
   }
 
   return (
@@ -26,7 +42,7 @@ function QuoteApp() {
         isDisplay
           ? <div>
             <QuoteDisplay quote={quote} />
-            <button onClick={getQuote}>Nü quote</button>
+            <button onClick={getQuote2}>Nü quote</button>
           </div>
           : <button onClick={toggleDisplay}>Click here for an inspirational quote!</button>
       }
